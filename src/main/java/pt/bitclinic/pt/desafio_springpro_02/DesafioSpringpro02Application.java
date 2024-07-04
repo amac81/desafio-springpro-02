3f46c7b390b84e4e1353790e44e7e94192633676
amac81 <arnaldocanelas@gmail.com>
2024-07-04 16:33:16 +0100

    Project created

---
+++ src/main/java/pt/bitclinic/pt/desafio_springpro_02/DesafioSpringpro02Application.java
@@ -0,0 +1,13 @@
+package pt.bitclinic.pt.desafio_springpro_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringpro02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringpro02Application.class, args);
+	}
+
+}
