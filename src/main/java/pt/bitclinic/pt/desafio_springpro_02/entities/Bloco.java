20f86441a2bfb823f6566a13cd7b1a5bc7644d61
amac81 <arnaldocanelas@gmail.com>
2024-07-05 09:40:52 +0100

    Class Bloco update - ORM mapping

--- src/main/java/pt/bitclinic/pt/desafio_springpro_02/entities/Bloco.java
+++ src/main/java/pt/bitclinic/pt/desafio_springpro_02/entities/Bloco.java
@@ -2,9 +2,23 @@ package pt.bitclinic.pt.desafio_springpro_02.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_bloco")
 public class Bloco {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant inicio;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant fim;
 	
 	public Bloco() {}
